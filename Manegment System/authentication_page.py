bfd7ffe06cf64623511936e45aaaaba15e004bdf
Iman Datta <dattaiman56@gmail.com>
2025-02-21 19:46:27 +0530

    authentication_page using class

--- Manegment System/authentication_page.py
+++ Manegment System/authentication_page.py
@@ -1,4 +1,4 @@
-class Authentication():
+class Authentication:
     def __init__(self):
         self.account = []
         self.sl_no = 0
@@ -7,26 +7,27 @@ class Authentication():
         print("1: Create Account")
         print("2: Login Account")
         print("3: Show Accounts Details")
-        print("4: Search Account")
-        print("5: Exit")
+        print("4: Exit")
         print("-" * 50)
         while True:
             try:
-                choice = int(input("Enter your choice (1 to 5): "))
-                if choice in [1,2,3,4,5]:
+                choice = int(input("Enter your choice (1 to 4): "))
+                if choice in [1,2,3,4]:
                     return choice
                 else:
-                    print("Please enter a valid option: (1 to 5)")
+                    print("Please enter a valid option: (1 to 4)")
             except ValueError:
-                print("Invalid input. Please enter a number (1-5).")
+                print("Invalid input. Please enter a number (1-4).")
 
     def create(self):
         print("-" * 50)
         print("------CREATE YOUR ACOOUNT------")
         while True:
             user_nm = input("Enter a unique user name: ")
-            if user_nm in self.account:
+            # if user_nm in self.account:
+            if any(user['usnm'] == user_nm for user in self.account): # from AI
                 print("This username already exists, try again")
+                break
             else:
                 fnm = input("Enter your first name: ")
                 lnm = input("Enter your last name: ")
@@ -42,6 +43,7 @@ class Authentication():
                     except ValueError:
                         print("Your password should in numbers.")            
                 user: dict = {
+                'usnm' : user_nm,
                 'sl' : self.sl_no,
                 'fnm' : fnm,
                 'lnm' : lnm,
@@ -51,14 +53,39 @@ class Authentication():
             print(f"Account created successfully for user: {fnm}!")
             break
 
+    # def login(self):
+    #     while True:
+    #         usnm = input("Enter your user name: ")
+    #         if any(user['usnm'] not in usnm for user in self.account):
+    #             print("Does not exit")
+    #         # if usnm in self.account:
+    #         else:
+    #             while True:
+    #                 try:
+    #                     passw = int(input("Enter your password: "))
+    #                 except ValueError:
+    #                     print("Your password should in numbers.")
+    #                     if passw == self.account[passw]:
+    #                         print(f"Welcome back {self.account['fnm']}")
+
     def login(self):
+        usnm = input("Enter your user name: ")
+        user = next((user for user in self.account if user['usnm'] == usnm), None)
+
+        if not user:
+            print("Username does not exist. Try again.")
+            return
+
         while True:
-            usnm = input("Enter your user name: ")
-            if usnm in self.account:
-                pass
-            else:
-                print("Does not exit")
-            passw = int(input("Enter your password: "))
+            try:
+                passw = int(input("Enter your password: "))
+                if passw == user['passw']:
+                    print(f"Welcome back {user['fnm']}!")
+                    return
+                else:
+                    print("Incorrect password. Try again.")
+            except ValueError:
+                print("Your password should be in numbers.")
 
     def show(self):
         if not self.account:  # Access the class variable directly
@@ -74,7 +101,6 @@ class Authentication():
 
 
 
-
 if __name__ == "__main__":
     acc = Authentication()
     while True:
@@ -85,56 +111,5 @@ if __name__ == "__main__":
             acc.login()
         elif choice == 3:
             acc.show()
-        elif choice == 4:
-            pass
         else:
             break
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def log_in(user_dic):
-#     user_name = input("Enter your username: ")
-#     if not user_dic[user_name]:
-#         print("Username not found. Please try again.")
-#         return
-
-#     paswd = input("Enter your password: ")
-#     if paswd == user_dic[user_name]["password"]:
-#         print(f"Login successful. Welcome back, {user_dic[user_name]['first_name']}!")
-#     else:
-#         print("Incorrect password. Please try again.")
-
-
-# def users_details(user_dic):
-#     if not user_dic:
-#         print("No users registered yet.")
-#         return
-
-#     print("\nRegistered Users:")
-#     for username, details in user_dic.items(): # Problem
-#         print(f"Username: {username}, Name: {details['first_name']} {details['last_name']}")
-
-
-# if __name__ == "__main__":
-#     user_dic = {}
-#     while True:
-#         choice = menu()
-#         if choice == 1:
-#             create_account(user_dic)
-#         elif choice == 2:
-#             log_in(user_dic)
-#         elif choice == 3:
-#             users_details(user_dic)
-#         elif choice == 4:
-#             print("Exiting program. Goodbye!")
-#             break
