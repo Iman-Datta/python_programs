14d369eb30b0645de6caf1dfcce635dccef7cad4
Iman Datta <dattaiman56@gmail.com>
2025-12-14 16:14:12 +0530

    Basic and string of python for recap

---
+++ Data science/01_pythonBasic.py
@@ -0,0 +1,15 @@
+a = 5
+b = 10
+print(a)
+print(f"The sum of a and b is: {a + b}")
+print("Hello world") # it is a comment
+
+print(type(a))
+
+name = "Iman"
+print(type(name))
+
+x = "4", y = "5"
+print(x+y)
+
+print(int(a) + int(b)) # Type casting
---
+++ Data science/02_string.py
@@ -0,0 +1,17 @@
+name = "Iman Datta"
+
+print(name.upper())      # Converts all characters in the string to uppercase
+
+print(name.lower())      # Converts all characters in the string to lowercase
+
+print(name.count("a"))   # Counts how many times the character 'a' appears in the string
+
+print(len(name))         # Returns the total number of characters in the string (including spaces)
+
+print(name[0:3])         # Extracts characters from index 0 up to (but not including) index 3
+
+print(name[1:-1])        # Extracts characters starting from index 1 up to the second-last character
+
+print(name[1:9])         # Extracts characters from index 1 up to (but not including) index 9
+
+print(name[-9:-1])       # Extracts characters from index -9 up to (but not including) index -1
